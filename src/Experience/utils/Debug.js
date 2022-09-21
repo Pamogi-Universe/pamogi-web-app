4a3c774894771886aa5473df2052b1f08fbef3e2
--global <ghayasleo99@gmail.com>
2022-09-21 14:58:46 +0500

    Added objects in sidebar and styled them. Also added gui control for toggling view and edit mode.

--- src/Experience/utils/Debug.js
+++ src/Experience/utils/Debug.js
@@ -3,5 +3,10 @@ import * as dat from 'lil-gui'
 export default class Debug {
   constructor() {
     this.ui = new dat.GUI();
+    this.obj = {
+      viewOnly: false
+    }
+
+    this.ui.add(this.obj, "viewOnly", { View: true, Edit: false }).name("Toggle View/Edit")
   }
 }
