bd45911a67474fb6585bab63504fb32ed1662579
--global <ghayasleo99@gmail.com>
2022-10-24 12:30:02 +0500

    Cleared script file

--- src/script.js
+++ src/script.js
@@ -1,7 +1,4 @@
 import Experience from './Experience'
 import './style.css'
 
-console.time("time")
-const experience = new Experience("#webgl")
-// console.log(experience)
-// console.timeEnd("time")
+// new Experience("#webgl")
